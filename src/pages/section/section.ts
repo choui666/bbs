21c324e7df870b3d38344b66ae7182655c33fac4
choui <zw454475525@sina.com>
2017-08-28 19:32:08 +0800

    1 登录拦截 2 主题查询

---
+++ src/pages/section/section.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import {IndexState} from "../../providers/reducer/BbsReducer";
+import {ServiceProvider} from "../../providers/service/service";
+import {SectionState} from "../../providers/reducer/SectionReducer";
+import {Store} from "@ngrx/store";
+import {AppState} from "../../providers/reducer/index";
+
+/**
+ * Generated class for the SectionPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-section',
+  templateUrl: 'section.html',
+})
+export class SectionPage {
+
+  game:IndexState = {
+    icon:'',
+    title:'',
+    id:''
+  };
+
+  icons:string = 'camera';
+
+  data:SectionState;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,private service:ServiceProvider,private store:Store<AppState>) {
+
+  }
+
+  ionViewDidLoad() {
+    this.game = this.navParams.get('game');
+    this.service.getSectionData({sectionid:this.game.id});
+    this.store.select('section').subscribe(res=>{
+      this.data = res;
+    })
+  }
+
+}
